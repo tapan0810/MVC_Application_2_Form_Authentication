b7b8d7d05601eb3e326ea998197107d21e763c7e
tapan0810 <arceus0810@gmail.com>
2026-08-23 12:13:00 +0530

    feat: Implemneted Data DbContext and added connectionstring tyo web.config

---
+++ MVC_Application_2_/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using MVC_Application_2_.Models;
+
+namespace MVC_Application_2_.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext():base("DefaultConnection")
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
